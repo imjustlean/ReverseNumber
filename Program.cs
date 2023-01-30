cd5cd4be1b88d60aad2e10de7c823d21c3b83eff
Leandro <Leandro@DESKTOP-V6R8B2D>
2023-01-30 19:39:34 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number="", numbers="";
+            Console.WriteLine("Enter number: ");
+            number = Console.ReadLine();
+            int z = number.Length-1;
+
+            for(int i =z; i>=0; i--)
+            {
+                numbers = numbers + number[i];
+            }
+            Console.WriteLine("Reverse  Order : {0}", numbers);
+            Console.Write("\n");
+            Console.ReadLine();
+           
+        }
+    }
+}
